6279acfdb608bbe05437e7c28a04ffc9672c25b9
Judson Fisher <jf225@humboldt.edu>
2024-03-19 15:20:38 -0700

    change shape of total_load

--- analysis/scripts/get_fuel_data.r
+++ analysis/scripts/get_fuel_data.r
@@ -1,23 +1,23 @@
 # function to access mostly raw data, but with combined veg and thoushr fuels.
 # and select the output variables (`...`)
-load2 <- function(shape = "wide", ...) {
-  load_vars <- c("onehr", "tenhr", "hundhr", "dufflitter", "thoushr", "veg")
-  tl <- pivot_wider(total_load, names_from = class, values_from = load) |>
-    mutate(
-      thoushr = rowSums(pick(c(thoushr_s, thoushr_r)), na.rm = TRUE),
-      veg = rowSums(pick(c(woody, herb)), na.rm = TRUE),
-      .keep = "unused"
-    ) |>
-    mutate(treatment = forcats::fct_relevel(treatment, c("gs", "ld", "ha", "hd")))
+load2 <- function(shape = "wide", pct = c("pre", "post", "both"), ...) {
+  pct <- pct[1]
+  pct_filter <- switch(pct,
+    pre = "prepct", post = "postpct", both = "prepct|postpct"
+  )
+  tl <- filter(total_load, phase == pct_filter)
+  load_vars <- c("onehr", "tenhr", "hundhr", "dufflitter", "thoushr", "veg", "veg_diff")
+  # TODO: if(pct == "post")
   if (!missing(...)) tl <- select(tl, ...)
   if (shape == "long") {
     tl <- pivot_longer(tl,
-      -any_of(c("site", "treatment", "corner", "azi")),
+      -any_of(c("phase", "site", "treatment", "corner", "azi")),
       names_to = "class",
       values_to = "load"
     )
     load_vars <- load_vars[load_vars %in% tl$class]
-    tl <- mutate(tl, class = factor(class, levels = load_vars))
+    tl <- mutate(tl, class = factor(class, levels = load_vars)) |>
+      filter(!is.na(load))
   }
   tl
 }
