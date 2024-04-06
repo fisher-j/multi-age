56e1beb667c32cf9f469ec55b6c85f34b2715187
Judson Fisher <jf225@humboldt.edu>
2024-04-05 17:30:22 -0700

    touched up regen analysis figures

---
+++ analysis/_notes.md
@@ -0,0 +1,6 @@
+# Notes
+
+## figs to include in thesis
+
+- Raw data density plot for sprout heights. Paragraph above.
+
--- analysis/scratch.r
+++ analysis/scratch.r
@@ -32,3 +32,51 @@ adonis2(Y ~ NO3, data = dat, permutations = 199)
 ## Correct with strata
 with(dat, adonis2(Y ~ NO3, data = dat, permutations = 199, strata = field))
 
+p1 + p2
+plot(ht10emm, comparisons = TRUE)
+emmcomp <- plot(ht10emm, comparisons = TRUE, plotit = FALSE) |> as_tibble()
+
+dht10 |>
+  ggplot(aes(treat, ht, color = spp, fill = spp)) +
+  facet_grid(~spp, switch = "x") +
+  theme(panel.spacing = unit(0, "lines"),
+        strip.background = element_blank(),
+        strip.text = element_blank(),
+        ) +
+  geom_dots() +
+  scale_color_brewer(palette = "Set2", aesthetics = c("color", "fill")) +
+  geom_pointrange(
+    data = emmcomp,
+    aes(y = the.emmean, ymin = lower.CL, ymax = upper.CL),
+    color = "gray60",
+    position = position_nudge(x = -0.07),
+    size = 0.7,
+    linewidth = 2.3,
+    show.legend = FALSE
+  ) +
+  geom_segment(
+    data = emmcomp,
+    aes(y = the.emmean, xend = treat, yend = rcmpl),
+    color = "dodgerblue4",
+    lineend = "butt",
+    linejoin = "bevel",
+    arrow = arrow(length = grid::unit(2.5, "mm")),
+    position = position_nudge(x = -0.07),
+    linewidth = 1
+  ) +
+  geom_segment(
+    data = emmcomp,
+    aes(y = the.emmean, xend = treat, yend = lcmpl),
+    color = "dodgerblue4",
+    lineend = "butt",
+    linejoin = "bevel",
+    arrow = arrow(length = grid::unit(2.5, "mm")),
+    position = position_nudge(x = -0.07),
+    linewidth = 1
+  )
+
+scales::show_col(colors()[!grepl("gr[ea]y", colors())], cex_label = 0.5)
+
+
+emmeans( mht18, spec = "treat", by = c("spp"), 
+  at = list(year = "10"), type = "response") |> pairs()
